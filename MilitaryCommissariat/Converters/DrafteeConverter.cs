61925069ae5bc3601cef2ee26233055990d1e24f
lephant <levb97@yandex.ru>
2017-09-23 15:03:15 +0300

    Создал конвертер

---
+++ MilitaryCommissariat/Converters/DrafteeConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using MilitaryCommissariat.Domain;
+
+namespace MilitaryCommissariat.Converters
+{
+    public class DrafteeConverter : IConverter<List<Draftee>, DataTable>
+    {
+        public List<Draftee> Convert(DataTable source)
+        {
+            List<Draftee> result = new List<Draftee>(source.Rows.Count);
+            foreach (DataRow dataRow in source.Rows)
+            {
+                Draftee draftee = new Draftee();
+                draftee.Id = (long) dataRow["id"];
+                draftee.LastName = (string) dataRow["last_name"];
+                draftee.FirstName = (string) dataRow["first_name"];
+                draftee.Patronymic = (string) dataRow["patronymic"];
+                draftee.BirthDate = (DateTime) dataRow["birth_date"];
+                draftee.Category = (string) dataRow["category"];
+                draftee.TroopType = (string) dataRow["troop_type"];
+                result.Add(draftee);
+            }
+            return result;
+        }
+    }
+}
---
+++ MilitaryCommissariat/Converters/IConverter.cs
@@ -0,0 +1,7 @@
+﻿namespace MilitaryCommissariat.Converters
+{
+    public interface IConverter<out TTarget, in TSource>
+    {
+        TTarget Convert(TSource source);
+    }
+}
