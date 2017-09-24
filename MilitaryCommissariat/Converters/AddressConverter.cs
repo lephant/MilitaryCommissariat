f303d8eb184cff6d133bb159769d9ce805226988
lephant <levb97@yandex.ru>
2017-09-24 18:39:32 +0300

    Реализовал выборку и конвертирование адресов, а так же обновление записи в бд

---
+++ MilitaryCommissariat/Converters/AddressConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+using MilitaryCommissariat.Domain;
+
+namespace MilitaryCommissariat.Converters
+{
+    public class AddressConverter : IConverter<Address, DataTable>
+    {
+        public Address Convert(DataTable source)
+        {
+            if (source.Rows.Count != 1)
+            {
+                throw new Exception("Число строк в переданной таблице отличается от 1");
+            }
+            DataRow dataRow = source.Rows[0];
+            Address result = new Address();
+            result.DrafteeId = (long) dataRow["draftee_id"];
+            result.MunicipalDistrict = Convert(dataRow["municipal_district"]);
+            result.MailIndex = Convert(dataRow["mail_index"]);
+            result.StreetName = Convert(dataRow["street_name"]);
+            result.HouseNumber = Convert(dataRow["house_number"]);
+            result.HousingNumber = Convert(dataRow["housing_number"]);
+            result.Apartment = Convert(dataRow["apartment"]);
+            result.HomePhone = Convert(dataRow["home_phone"]);
+            return result;
+        }
+
+        private string Convert(object source)
+        {
+            if (System.Convert.IsDBNull(source))
+            {
+                return "";
+            }
+            return (string) source;
+        }
+    }
+}
--- MilitaryCommissariat/DAO/AddressDao.cs
+++ MilitaryCommissariat/DAO/AddressDao.cs
@@ -1,10 +1,93 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Data;
 using System.Text;
+using MilitaryCommissariat.Domain;
+using MilitaryCommissariat.Utils;
+using MySql.Data.MySqlClient;
 
 namespace MilitaryCommissariat.DAO
 {
-    class AddressDao
+    public class AddressDao
     {
+        public DataTable GetByDraftee(long id)
+        {
+            try
+            {
+                StringBuilder sqlBuilder = new StringBuilder();
+                sqlBuilder
+                    .Append("SELECT ")
+                    .Append("addr.draftee_id draftee_id, ")
+                    .Append("addr.municipal_district municipal_district, ")
+                    .Append("addr.mail_index mail_index, ")
+                    .Append("addr.street_name street_name, ")
+                    .Append("addr.house_number house_number, ")
+                    .Append("addr.housing_number housing_number, ")
+                    .Append("addr.apartment apartment, ")
+                    .Append("addr.home_phone home_phone ")
+                    .Append("FROM addresses addr ")
+                    .Append("WHERE draftee_id=")
+                    .Append(id)
+                    .Append(";");
+                var dataAdapter = new MySqlDataAdapter(sqlBuilder.ToString(), ConnectionUtils.GetConnection());
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+                return dataTable;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public DataTable GetByDraftee(Draftee draftee)
+        {
+            return GetByDraftee(draftee.Id);
+        }
+
+        public DataTable GetByDraftee(TableDraftee tableDraftee)
+        {
+            return GetByDraftee(tableDraftee.Id);
+        }
+
+        public void Update(Address address)
+        {
+            MySqlConnection connection = ConnectionUtils.GetConnection();
+            try
+            {
+                StringBuilder sqlBuilder = new StringBuilder();
+                sqlBuilder
+                    .Append("UPDATE addresses SET ")
+                    .Append("municipal_district='")
+                    .Append(address.MunicipalDistrict)
+                    .Append("', ")
+                    .Append("mail_index='")
+                    .Append(address.MailIndex)
+                    .Append("', ")
+                    .Append("street_name='")
+                    .Append(address.StreetName)
+                    .Append("', ")
+                    .Append("house_number='")
+                    .Append(address.HouseNumber)
+                    .Append("', ")
+                    .Append("housing_number='")
+                    .Append(address.HousingNumber)
+                    .Append("', ")
+                    .Append("apartment='")
+                    .Append(address.Apartment)
+                    .Append("', ")
+                    .Append("home_phone='")
+                    .Append(address.HomePhone)
+                    .Append("' ")
+                    .Append("WHERE draftee_id=")
+                    .Append(address.DrafteeId)
+                    .Append(";");
+                MySqlCommand command = new MySqlCommand(sqlBuilder.ToString(), connection);
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
     }
 }
