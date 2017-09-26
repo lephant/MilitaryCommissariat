fdef7c65fc54230e7e397efc148454612aff30a3
lephant <levb97@yandex.ru>
2017-09-26 09:48:37 +0300

    Добавил кнопки и обработчики. Добавил инициализацию

--- MilitaryCommissariat/Windows/DrafteeWindow.xaml.cs
+++ MilitaryCommissariat/Windows/DrafteeWindow.xaml.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using MilitaryCommissariat.Converters;
+using MilitaryCommissariat.DAO;
 
 namespace MilitaryCommissariat.Windows
 {
@@ -22,6 +15,43 @@ namespace MilitaryCommissariat.Windows
         public DrafteeWindow()
         {
             InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            var dao = new DrafteeDao();
+            var converter = new DrafteeConverter();
+            var draftee = converter.Convert(dao.GetById(DrafteeId));
+            FirstNameValueLabel.Content = draftee.FirstName;
+            LastNameValueLabel.Content = draftee.LastName;
+            PatronymicValueLabel.Content = draftee.Patronymic;
+            BirthDateValueLabel.Content = draftee.BirthDate.ToString("yyyy.MM.dd");
+        }
+
+        private void ReturnButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void SelectButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void CommonInfoItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+        }
+
+        private void EducationItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+        }
+
+        private void RelativeItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+        }
+
+        private void DocumentItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
         }
     }
 }
