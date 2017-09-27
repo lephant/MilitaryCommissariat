45317768119875a1ff341a8eecef142e9683a065
lephant <levb97@yandex.ru>
2017-09-27 20:06:06 +0300

    Добавил подгрузку данных. Реализовал прокрутку содержимого

--- MilitaryCommissariat/Windows/DrafteeEducationWindow.xaml.cs
+++ MilitaryCommissariat/Windows/DrafteeEducationWindow.xaml.cs
@@ -9,6 +9,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MilitaryCommissariat.Controls;
+using MilitaryCommissariat.Converters;
+using MilitaryCommissariat.DAO;
 
 namespace MilitaryCommissariat.Windows
 {
@@ -22,6 +25,29 @@ namespace MilitaryCommissariat.Windows
         public DrafteeEducationWindow()
         {
             InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            var drafteeDao = new DrafteeDao();
+            var drafteeConverter = new DrafteeConverter();
+            var draftee = drafteeConverter.Convert(drafteeDao.GetById(DrafteeId));
+            FirstNameValueLabel.Content = draftee.FirstName;
+            LastNameValueLabel.Content = draftee.LastName;
+            PatronymicValueLabel.Content = draftee.Patronymic;
+            BirthDateValueLabel.Content = draftee.BirthDate.ToString("yyyy.MM.dd");
+
+            var educationPlaceDao = new EducationPlaceDao();
+            var educationPlaceConverter = new EducationPlaceListConverter();
+            var educations = educationPlaceConverter.Convert(educationPlaceDao.GetListByDraftee(draftee.Id));
+            foreach (var educationPlace in educations)
+            {
+                var view = new EducationPlaceView();
+                view.CollapseButtons = false;
+                view.EducationPlace = educationPlace;
+                EducationPlacePanel.Children.Add(view);
+            }
         }
     }
 }
