ce3831f0f1b8b5ffdcc474801e55e2ef37dd91a8
lephant <levb97@yandex.ru>
2017-09-28 15:32:54 +0300

    Создал пустые окна для редактирования

---
+++ MilitaryCommissariat/Windows/EditDrafteeEducationWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MilitaryCommissariat.Windows
+{
+    /// <summary>
+    /// Interaction logic for EditDrafteeEducationWindow.xaml
+    /// </summary>
+    public partial class EditDrafteeEducationWindow : Window
+    {
+        public EditDrafteeEducationWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
