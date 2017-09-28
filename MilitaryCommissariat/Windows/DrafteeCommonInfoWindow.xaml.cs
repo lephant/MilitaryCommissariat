33ccc3af885c6802556c37fc5420e1a600890892
lephant <levb97@yandex.ru>
2017-09-28 20:07:28 +0300

    Реализовал заполнение данных и скроллинг их

--- MilitaryCommissariat/Windows/DrafteeCommonInfoWindow.xaml.cs
+++ MilitaryCommissariat/Windows/DrafteeCommonInfoWindow.xaml.cs
@@ -9,6 +9,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MilitaryCommissariat.Converters;
+using MilitaryCommissariat.DAO;
+using MilitaryCommissariat.Domain;
 
 namespace MilitaryCommissariat.Windows
 {
@@ -22,6 +25,53 @@ namespace MilitaryCommissariat.Windows
         public DrafteeCommonInfoWindow()
         {
             InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            FillData(GetCurrentDraftee());
+            FillData(GetCurrentAddress());
+        }
+
+        private Draftee GetCurrentDraftee()
+        {
+            var dao = new DrafteeDao();
+            var converter = new DrafteeConverter();
+            return converter.Convert(dao.GetById(DrafteeId));
+        }
+
+        private Address GetCurrentAddress()
+        {
+            var dao = new AddressDao();
+            var converter = new AddressConverter();
+            return converter.Convert(dao.GetByDraftee(DrafteeId));
+        }
+
+        private void FillData(Draftee draftee)
+        {
+            FirstNameValueLabel.Content = draftee.FirstName;
+            LastNameValueLabel.Content = draftee.LastName;
+            PatronymicValueLabel.Content = draftee.Patronymic;
+            BirthDateValueLabel.Content = draftee.BirthDate.ToString("yyyy.MM.dd");
+            BirthPlaceValueLabel.Text = draftee.BirthPlace;
+        }
+
+        private void FillData(Address address)
+        {
+            MunicipalDistrictValueLabel.Text = address.MunicipalDistrict;
+            MailIndexValueLabel.Text = address.MailIndex;
+            StreetNameValueLabel.Text = address.StreetName;
+            HouseNumberValueLabel.Text = address.HouseNumber;
+            HousingNumberValueLabel.Text = address.HousingNumber;
+            ApartmentValueLabel.Text = address.Apartment;
+            HomePhoneValueLabel.Text = address.HomePhone;
+        }
+
+        public void Refresh()
+        {
+            FillData(GetCurrentDraftee());
+            FillData(GetCurrentAddress());
         }
     }
 }
